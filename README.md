219b02999e1f8288c88316c9acf28dcebe51ed0d
qood1982 <majed.m.h@gmail.com>
2026-03-29 07:46:19 +0300

    Refactor code structure for improved readability and maintainability

--- README.md
+++
@@ -1,1 +0,0 @@
-# ZWCM-Arabic-English
--- dist/assets/input-3Av4L4wJ.js
+++
@@ -1,6 +0,0 @@
-import{c as i,a as s}from"./index-DUJC9naY.js";import{j as n}from"./ui-Cfn8IxmW.js";import{r as a}from"./vendor-CpZmRmQi.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const u=i("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]),c=a.forwardRef(({className:e,type:r,...o},t)=>n.jsx("input",{type:r,className:s("flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),ref:t,...o}));c.displayName="Input";export{u as C,c as I};
